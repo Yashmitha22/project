b6528f1305f594367b539d45a4bc0f86f91f0e80
YASHMITHA A <yashmithavagman@gmail.com>
2025-05-28 21:14:45 +0530

    commit

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 function myNav(){
     let bar = document.getElementById("bar");
+    let nav = document.querySelector(".navigation");
+    bar.onclick = ()=>{
+        if(nav.style.right == "10%"){
+            nav.style.right - "-10%";
+            bar.src = "photos/img4.jpeg"
+        }else{
+            nav.style.right = "0%";
+            bar.src = "photos/image3bt.jpeg"
+        }
+    }
 }
+myNav();
